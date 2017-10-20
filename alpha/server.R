f817dd9834b65340959e3121821397ffb0dd5841
Akiff Manji <amanji@dhcp-e8-b1-fc-93-41-da.ubcvisitor.wifi.ubc.ca>
2017-10-20 16:05:52 -0700

    Integrated alpha diversity analyses into app

--- alpha/server.R
+++ alpha/server.R
@@ -3,15 +3,14 @@
 alphaServer <- function(input, output, session) {
     # TODO: Put Server logic here
     output$phylo <- renderPlot({
-    title <- "Alpha Diversity Metrics"
-    
-    if (input$dataSelection == "GlobalPatterns") {
-      data("GlobalPatterns")
-      plot_richness(GlobalPatterns, measures = c("Observed", input$groupSelection))
-      
-    } else if (input$dataSelection == "esophagus") {
-      data("esophagus")
-      plot_richness(esophagus, measures = c("Observed", input$groupSelection))
-    }
-  })
+        title <- "Alpha Diversity Metrics"
+
+        if (input$dataSelection == "global") {
+            data("GlobalPatterns")
+            plot_richness(GlobalPatterns, measures = c("Observed", input$groupSelection))
+        } else if (input$dataSelection == "esophagus") {
+            data("esophagus")
+            plot_richness(esophagus, measures = c("Observed", input$groupSelection))
+        }
+    })
 }
--- alpha/ui.R
+++ alpha/ui.R
@@ -15,16 +15,23 @@ alphaUI <- function(id) {
 
                 # Sidebar panel
                 sidebarPanel(
-                    # TODO: Sidebar panel UI logic here
+                    selectInput(
+                        inputId = ns("dataSelection"),
+                        label = "Choose a Dataset",
+                        choices = c("Global Patterns" = "global", "Esophagus" = "esophagus"),
+                        selected = "global"
+                    ),
+                    checkboxGroupInput(
+                        inputId = ns("groupSelection"),
+                        label = "Select Metrics",
+                        choiceNames = c("ACE", "Shannon", "Simpson", "InvSimpson", "Fisher"),
+                        choiceValues = c("ACE", "Shannon", "Simpson", "InvSimpson", "Fisher")
+                    )
                 ),
 
                 # Main panel
                 mainPanel(
-                    # TODO: Main  panel UI logic here
-                    #Roshan added
-                    selectInput(inputId = "dataSelection", label = "Choose a Dataset", multiple = FALSE, choices = c("GlobalPatterns", "esophagus"), selected = "GlobalPatterns"), 
-                    checkboxGroupInput(inputId = "groupSelection", label = "Select Metrics", choiceNames = c("ACE", "Shannon", "Simpson", "InvSimpson", "Fisher"), choiceValues = c("ACE", "Shannon", "Simpson", "InvSimpson", "Fisher")), 
-                    plotOutput(outputId = "phylo")
+                    plotOutput(outputId = ns("phylo"))
                 )
             )
         )

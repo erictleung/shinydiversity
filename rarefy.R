a47c7af4b633675b3e793da89258d3f89a33e74b
aimirza <alowi33@gmail.com>
2017-10-22 09:56:22 -0700

    Add file to rarefy samples

--- rarefy.R
+++ rarefy.R
@@ -3,9 +3,8 @@
 # sample size. 
 # Optionally, the count paramater may be set to a desired sample size (integer)
 
-
 rarefy <- function(otu_table, count, min=TRUE) {
-  otu_table=otu_table(otu_table, taxa_are_rows = TRUE)
+  #otu_table=otu_table(otu_table, taxa_are_rows = TRUE)
   if(missing(count)) {
     rarefy_even_depth(otu_table, replace = FALSE) #defult is to rarefy to the smallest sample size
   } else {
@@ -13,5 +12,7 @@ rarefy <- function(otu_table, count, min=TRUE) {
   }
 }
 
-# otu_table: (required) An otu table with rows are OTUs and columns as samples. Not a pyloseq-class.
+# otu_table: (required) An otu table with rows are OTUs and columns as samples.
 # count:     (optional) A single integer value equal to the number of reads being simulated, also known as the depth
+# use 'sample_sums()' to get sample size info
+

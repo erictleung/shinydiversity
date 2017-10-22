3e90ea22a0b86c4e6da98a15c4d8e1055c0f1b74
Roshan Noronha <rnoronha00@users.noreply.github.com>
2017-10-22 10:48:52 -0700

    Update server.R

--- alpha/server.R
+++ alpha/server.R
@@ -4,21 +4,18 @@ alphaServer <- function(input, output, session) {
     # TODO: Put Server logic here
     output$phylo <- renderPlot({
         title <- "Alpha Diversity Metrics"
-        selected_theme<-switch(input$plotTheme,
-                               "Grey theme" = theme_gray() ,
-                               "Black & White theme" =theme_bw(),
-                               "Classic theme" = theme_classic()
-
-        )
-
-        theme_set(selected_theme)
-
-        if (input$dataSelection == "global") {
-            data("GlobalPatterns")
-            plot_richness(GlobalPatterns, measures = c("Observed", input$groupSelection))
-        } else if (input$dataSelection == "esophagus") {
-            data("esophagus")
-            plot_richness(esophagus, measures = c("Observed", input$groupSelection))
-        }
+        otuMatrix <- loadOTUMatrix(input$dataSelection)
+        plot_richness(get(input$dataSelection), measures = c("Observed", input$groupSelection))
     })
+  
+    output$heatmap <- renderPlot({
+        heatmap(otuMatrix, Colv = NA, Rowv = NA, scale = "row", labRow = FALSE, main = input$dataSelection)
+    })
+  
+    output$bar <- renderPlot({
+        title <- "dfadfa"
+        barplot(otuMatrix[, input$sample], names.arg = rownames(otuMatrix), main = colnames(otuMatrix)[input$sample])
+    })
+  
+    output$table <- renderTable(loadSDTable(input$dataSelection), rownames = TRUE)
 }

2c59aff0be50159a1503802a0d6b0f7764b6c2dd
Roshan Noronha <rnoronha00@users.noreply.github.com>
2017-10-22 10:46:56 -0700

    Update ui.R

--- alpha/ui.R
+++ alpha/ui.R
@@ -1,4 +1,39 @@
-# Alpha Module UI funciton
+#load phyloseq datasets
+data("GlobalPatterns")
+data("esophagus")
+
+#load preset dataset
+loadOTUMatrix <- function(dataSetName) {
+  
+  otuTable <- otu_table(get(dataSetName))
+  otuMatrix <- matrix(nrow = nrow(otuTable), ncol = 0)
+  otuMatrix <- apply(otuTable, 2, function(x) cbind(otuMatrix, x))
+  rownames(otuMatrix) <- rownames(otu_table(get(dataSetName)))
+  colnames(otuMatrix) <- colnames(otu_table(get(dataSetName)))
+  otuMatrix
+}
+
+#create preset singleton/doubleton table
+loadSDTable <- function(dataSetName) {
+  
+  otuData <- loadOTUMatrix(dataSetName)
+  
+  sdTable <- matrix(nrow = 2, ncol = length(colnames(otuData)))
+  
+  rownames(sdTable) <- c("Singleton", "Doubleton")
+  colnames(sdTable) <- colnames(otuData)
+  
+  sdTable[1,] <- apply(otuData, 2, function(x) length(which(x == 1)))
+  sdTable[2,] <- apply(otuData, 2, function(x) length(which(x == 2)))
+  
+  t(sdTable)
+}
+
+#generate initial data
+otuMatrix <- loadOTUMatrix("GlobalPatterns")
+sdTable <- loadSDTable("GlobalPatterns")
+
+# Alpha Module UI function
 alphaUI <- function(id) {
     # Create a namespace function using the provided ID
     ns <- NS(id)
@@ -6,40 +41,13 @@ alphaUI <- function(id) {
     # NOTE: All referenced input variables must be wrapped with a call to ns(), ex. ns("someInputID")
 
     # Define the UI for the app
-    tagList(
-        fluidPage(
-            titlePanel("Alpha Diversiy"),
-
-            # Sidebar layout
-            sidebarLayout(
-
-                # Sidebar panel
-                sidebarPanel(
-                    selectInput(
-                        inputId = ns("dataSelection"),
-                        label = "Choose a Dataset",
-                        choices = c("Global Patterns" = "global", "Esophagus" = "esophagus"),
-                        selected = "global"
-                    ),
-                    checkboxGroupInput(
-                        inputId = ns("groupSelection"),
-                        label = "Select Metrics",
-                        choiceNames = c("ACE", "Shannon", "Simpson", "InvSimpson", "Fisher"),
-                        choiceValues = c("ACE", "Shannon", "Simpson", "InvSimpson", "Fisher")
-                    ),
-                    selectInput(
-                        inputId = ns("plotTheme"),
-                        label = "Choose a theme for the Plots",
-                        choices = c("Grey theme", "Black & White theme","Classic theme"),
-                        selected = "Grey theme"
-                    )
-                ),
-
-                # Main panel
-                mainPanel(
-                    plotOutput(outputId = ns("phylo"))
-                )
-            )
-        )
-    )
+    titlePanel("Alpha Diversity Metrics"),
+    selectInput(inputId = "dataSelection", label = "Choose a Dataset", multiple = FALSE, choices = c("GlobalPatterns", "esophagus"), selected = "GlobalPatterns"), 
+    checkboxGroupInput(inputId = "groupSelection", label = "Select Metrics", choiceNames = c("ACE", "Shannon", "Simpson", "InvSimpson", "Fisher"), choiceValues = c("ACE", "Shannon", "Simpson", "InvSimpson", "Fisher")), 
+    plotOutput(outputId = "phylo"),
+    
+    titlePanel("Alpha Diversity Summary"),
+    tableOutput("table"),
+    sliderInput(inputId = "sample", label = "Slide to Move through The Samples", value = 1, step = 1, min = 1, max = length(colnames(otuMatrix))),
+    plotOutput(outputId = "heatmap"), plotOutput(outputId = "bar")
 }

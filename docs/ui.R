ece1dc6531f29d667742abd6be627bbb07896424
scatcher125 <shripp123@gmail.com>
2017-10-22 13:08:43 -0700

    Add tabs in docs -- updated (#45)
    * Dropdown menu in docs
    
    * Dropdown menu -- ui updated

--- docs/ui.R
+++ docs/ui.R
@@ -1,4 +1,4 @@
-docsUI <- function(id) {
+docsUIalpha <- function(id) {
     # Create a namespace function using the provided ID
     ns <- NS(id)
 
@@ -17,6 +17,31 @@ docsUI <- function(id) {
                 column(
                     width = 12,
                     withMathJax(includeMarkdown("docs/alpha.md"))
+                )
+            )
+        )
+    )
+}
+
+docsUIbeta <- function(id) {
+    # Create a namespace function using the provided ID
+    ns <- NS(id)
+
+    # Compile RMarkdown files
+    knit("docs/beta.Rmd", "docs/beta.md", quiet = TRUE)
+
+    # Define the UI for the app
+    tagList(
+        fluidPage(
+            # Application title
+            titlePanel("Technical Details on Calculations"),
+
+            # Sidebar with a slider input for number of bins
+            fluidRow(
+                # Show a plot of the generated distribution
+                column(
+                    width = 12,
+                    withMathJax(includeMarkdown("docs/beta.md"))
                 )
             )
         )

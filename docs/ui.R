1752fadcf103e513e2b50bc62b6b8fdd6d36cd94
Eric Leung <eric@erictleung.com>
2017-12-01 01:09:40 -0800

    Add table of contents for alpha documentation (#42)
    * Make application ShinyApps ready
    
    * Add table of contents for alpha documentation

--- docs/ui.R
+++ docs/ui.R
@@ -3,20 +3,17 @@ docsUIalpha <- function(id) {
     ns <- NS(id)
 
     # Compile RMarkdown files
-    knit("docs/alpha.Rmd", "docs/alpha.md", quiet = TRUE)
+    render("docs/alpha.Rmd", quiet = TRUE)
 
     # Define the UI for the app
     tagList(
         fluidPage(
-            # Application title
-            titlePanel("Technical Details on Calculations"),
-
             # Sidebar with a slider input for number of bins
             fluidRow(
                 # Show a plot of the generated distribution
                 column(
                     width = 12,
-                    withMathJax(includeMarkdown("docs/alpha.md"))
+                    withMathJax(includeHTML("docs/alpha.html"))
                 )
             )
         )

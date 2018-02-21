246898daf4c35ecf2bb2efc55d7b5026db1e2970
Eric Leung <eric@erictleung.com>
2018-02-21 12:28:43 -0800

    Render beta docs the same as alpha documentation

--- docs/ui.R
+++ docs/ui.R
@@ -25,20 +25,17 @@ docsUIbeta <- function(id) {
     ns <- NS(id)
 
     # Compile RMarkdown files
-    knit("docs/beta.Rmd", "docs/beta.md", quiet = TRUE)
+    render("docs/beta.Rmd", quiet = TRUE)
 
     # Define the UI for the app
     tagList(
         fluidPage(
-            # Application title
-            titlePanel("Technical Details on Calculations"),
-
             # Sidebar with a slider input for number of bins
             fluidRow(
                 # Show a plot of the generated distribution
                 column(
                     width = 12,
-                    withMathJax(includeMarkdown("docs/beta.md"))
+                    withMathJax(includeHTML("docs/beta.html"))
                 )
             )
         )
